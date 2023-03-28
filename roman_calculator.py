c9a9de1840efad679f5d68fab261d78c1e7ea110
PrOriginal <33231046+PrOriginal@users.noreply.github.com>
2023-03-28 14:44:38 +0200

    Update roman_calculator.py

--- roman_calculator.py
+++ roman_calculator.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 
-SMALL_FONT_STYLE = ("Courier", 16, "bold")
+SMALL_FONT_STYLE = ("Courier", 12, "bold")
 LARGE_FONT_STYLE = ("Courier", 30, "bold")
 DIGITS_BUTTONS_FONT_STYLE = ("Courier", 28, "bold")
 OTHER_BUTTONS_FONT_STYLE = ("Courier", 20, "bold")
@@ -13,10 +13,11 @@ class Calculator:
         self.root.geometry("360x540")
         self.root.resizable(True, False)
         self.root.title("Be like a Roman!")
-        #self.root.iconbitmap("icon.ico")
+        # self.root.iconbitmap("icon.ico")
         self.total_arabic_expression = ""
         self.total_roman_expression = ""
         self.current_expression = ""
+        self.is_roman_number_too_big = False
         self.display_frame = self.create_display_frame()
         self.arabic_total, self.roman_total, self.current = self.create_display_labels()
         self.buttons_frame = self.create_buttons_frame()
@@ -28,12 +29,9 @@ class Calculator:
             "V": (3, 1), "X": (3, 2), "L": (3, 3),
             "I": (4, 2)
         }
-        self.roman_nums = {1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX",
-                           70: "LXX", 80: "LXXX", 90: "XC", 100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM", 1000: "M"
-                           }
         self.nums_dictionary = {"I": 1, "V": 5, "X": 10,
                                 "L": 50, "C": 100, "D": 500, "M": 1000}
-      
+
         self.operations = {"÷": "/", "x": "*", "-": "-", "+": "+"}
         self.create_roman_digits_buttons()
         self.create_operation_buttons()
@@ -50,10 +48,10 @@ class Calculator:
 
     def create_display_labels(self):
         total_arabic_label = tk.Label(self.display_frame, text="0", anchor=tk.E,
-                                      bg="#EF4E50", fg="white", padx=24, font=SMALL_FONT_STYLE)
+                                      bg="#EF4E50", fg="white", padx=24, font=SMALL_FONT_STYLE,  wrapleng =340)
         total_arabic_label.pack(expand=True, fill="both")
         total_roman_label = tk.Label(self.display_frame, text="Nullus", anchor=tk.E,
-                                     bg="#F05F61", fg="white", padx=24, font=SMALL_FONT_STYLE)
+                                     bg="#F05F61", fg="white", padx=24, font=SMALL_FONT_STYLE, wrapleng =340)
         total_roman_label.pack(expand=True, fill="both")
         current_label = tk.Label(self.display_frame, text="Nullus", anchor=tk.E,
                                  bg="white", fg="black", padx=24, font=LARGE_FONT_STYLE)
@@ -65,10 +63,6 @@ class Calculator:
         frame = tk.Frame(self.root)
         frame.pack(expand=False, fill="both")
         return frame
-
-    def add_to_expression(self, value):
-        self.current_expression += value
-        self.update_current_label()
 
     def roman_to_arabic(self, roman_num):
         arabic_num = 0
@@ -83,12 +77,15 @@ class Calculator:
         return arabic_num
 
     def arabic_to_roman(self, result):
+        roman_nums = {1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX",
+                      70: "LXX", 80: "LXXX", 90: "XC", 100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM", 1000: "M"
+                      }
+
         arabic_num = int(round(result, 0))
         if arabic_num == 0:
             self.total_roman_expression += " = Nullus"
             self.current_expression = "Nullus"
             return self.total_roman_expression, self.current_expression
-
         roman_num = ""
         if arabic_num < 0:
             roman_num += "-"
@@ -96,7 +93,8 @@ class Calculator:
         if arabic_num//1000 != 0:
             count = arabic_num//1000
             if count > 10:
-                roman_num += f"(M*{count})"
+                roman_num += f"M*({count})"
+                self.is_roman_number_too_big = True
                 arabic_num -= 1000*count
             else:
                 for i in range(0, count):
@@ -105,16 +103,23 @@ class Calculator:
         if arabic_num//100 != 0:
             count = arabic_num//100*100
             arabic_num -= count
-            roman_num += self.roman_nums[count]
+            roman_num += roman_nums[count]
         if arabic_num//10 != 0:
             count = arabic_num//10*10
             arabic_num -= count
-            roman_num += self.roman_nums[count]
+            roman_num += roman_nums[count]
         if arabic_num > 0:
-            roman_num += self.roman_nums[arabic_num]
+            roman_num += roman_nums[arabic_num]
         self.total_roman_expression += f" = {roman_num}"
         self.current_expression = roman_num
-        return self.total_roman_expression, self.current_expression
+
+    def add_to_expression(self, value):
+        if self.is_roman_number_too_big:
+            self.current_expression =""
+            self.is_roman_number_too_big = False
+            self.update_current_label()
+        self.current_expression += value
+        self.update_current_label()
 
     def append_operator(self, operator):
         self.total_arabic_expression += str(
@@ -132,15 +137,12 @@ class Calculator:
             self.roman_to_arabic(self.current_expression))
         self.total_roman_expression += self.current_expression
 
-        # Evaluation process
         result = eval(self.total_arabic_expression)
         self.total_arabic_expression += f" = {str(result)}"
         self.arabic_to_roman(result)
-
         self.update_total_arabic_label()
         self.update_total_roman_label()
         self.update_current_label()
-
         self.total_arabic_expression = ""
         self.total_roman_expression = ""
 
@@ -157,8 +159,8 @@ class Calculator:
         self.current.config(text="Nullus")
 
     def help(self):
-        messagebox.showinfo(title=f"O tempŏra! O mores!", 
-            message="Be aware, that true Romans didn't use negative numerals and zero! Nor decimal point.\nHence all division results are rounded. But you still can get negative numbers.\n\nLabels:\nTop label presents expression in arabic numbers for understang what is goin on.\nSecond label - expression in roman numbers.")
+        messagebox.showinfo(title=f"O tempŏra! O mores!",
+                            message="Be aware, that true Romans didn't use negative numerals and zero! Nor decimal point.\nHence all division results are rounded. But you still can get negative numbers.\n\nLabels:\nTop label presents expression in arabic numbers for understang what is goin on.\nSecond label - expression in roman numbers.")
 
     def create_roman_digits_buttons(self):
         for d, grid in self.roman_buttons.items():
@@ -199,9 +201,11 @@ class Calculator:
         self.root.bind("<BackSpace>", lambda e: self.backspace())
         self.root.bind("<Delete>", lambda e: self.clear())
 
-        num_binds = {"I":1,"II":2,"III":3,"IV":4,"V": 5,"VI":6,"VII":7,"VII":8,"IX":9,"X":10} 
+        num_binds = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5,
+                     "VI": 6, "VII": 7, "VII": 8, "IX": 9, "X": 10}
         for key in num_binds:
-            self.root.bind(num_binds[key], lambda e, operator = key: self.add_to_expression(operator))
+            self.root.bind(
+                num_binds[key], lambda e, operator=key: self.add_to_expression(operator))
         for key in self.nums_dictionary:
             self.root.bind(key.lower(), lambda e,
                            digit=key: self.add_to_expression(digit))
